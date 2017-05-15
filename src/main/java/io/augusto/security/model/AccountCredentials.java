c3be40cab39cf102bb1aee416e9958be3f9b77a6
christopher.uribe <christopher.uribej@gmail.com>
2017-05-15 12:06:04 -0500

    Initial commit

---
+++ src/main/java/io/augusto/security/model/AccountCredentials.java
@@ -0,0 +1,26 @@
+package io.augusto.security.model;
+
+/**
+ * Created by Interware on 15/05/2017.
+ */
+public class AccountCredentials {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/io/augusto/security/restapi/UserCtrl.java
@@ -0,0 +1,16 @@
+package io.augusto.security.restapi;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserCtrl {
+
+    /* Maps to all HTTP actions by default (GET,POST,..)*/
+    @RequestMapping("/user")
+    public @ResponseBody String all() {
+        return "{\"users\":[{\"firstname\":\"Richard\", \"lastname\":\"Feynman\"}," +
+                "{\"firstname\":\"Marie\",\"lastname\":\"Curie\"}]}";
+    }
+}
